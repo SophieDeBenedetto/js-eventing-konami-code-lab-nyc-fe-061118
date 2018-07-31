3d9a2906e8912901e565b217c3c60a064c36f3c9
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2018-07-31 00:36:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+//jQuery
+// $("body").on("keydown", init)
 
+// "vanilla" JS 
+document.body.addEventListener("keydown", init)
+//const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
+function init(event) {
+  debugger;
+
+  let key = event.which;
+  if (key === code[index]) {
+    index++
+    if (index === code.length) {
+      alert("KONAMI")
+    }
+  } else {
+    index = 0;
+  }
 }
+
